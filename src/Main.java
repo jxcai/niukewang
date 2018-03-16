daba0fdfd5cc9eb1a8d446d5af09855ec2e4188f
caixiaojie <550971869@qq.com>
2018-03-16 17:36:15 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+     String s1 = new String("hello");
+     String s2 = new String("threr");
+     String s3 = new String("0");
+     s3 = s1 + s2;
+     System.out.println(s3);
+    }
+
+}
